eda62636e5c1767a0cd4eac3a1c4912b472bca0f
Ben Quinn <benjamesquinn@gmail.com>
2022-10-01 14:43:06 +0100

    Added calculateTax function and refactored

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 const { taxBands } = require("../src/data");
 
+const calculateTax = (value, rate) => Math.floor(value * rate);
+
 exports.calculateLBTT = function (transactionFee) {
   let tax = 0;
   let taxableTransactionFee = transactionFee - taxBands[0].difference;
-  console.log(taxableTransactionFee)
 
   if (!Number(transactionFee) || transactionFee < 0) {
     throw new Error(`Input must be a positive integer.`);
@@ -13,44 +14,37 @@ exports.calculateLBTT = function (transactionFee) {
     return tax;
 
     // band 1
-  } else if (
-    transactionFee > taxBands[1].range[0] &&
-    transactionFee < taxBands[1].range[1]
-  ) {
-    tax += Math.floor(taxableTransactionFee * taxBands[1].rate);
+  } else if (transactionFee > taxBands[1].range[0] && transactionFee < taxBands[1].range[1]) {
+    tax += calculateTax(taxableTransactionFee, taxBands[1].rate);
 
     // band 2
-  } else if (
-    transactionFee > taxBands[2].range[0] &&
-    transactionFee < taxBands[2].range[1]
+  } else if (transactionFee > taxBands[2].range[0] && transactionFee < taxBands[2].range[1]
   ) {
-    tax += taxBands[1].difference * taxBands[1].rate;
+    tax += calculateTax(taxBands[1].difference, taxBands[1].rate);
+    
     taxableTransactionFee -= taxBands[1].difference;
-    tax += taxableTransactionFee * taxBands[2].rate;
+    tax += calculateTax(taxableTransactionFee, taxBands[2].rate);
 
     // band 3
-  } else if (
-    transactionFee > taxBands[3].range[0] &&
-    transactionFee < taxBands[3].range[1]
-  ) {
-    tax += Math.floor(
-      taxBands[1].difference * taxBands[1].rate +
-        taxBands[2].difference * taxBands[2].rate
-    );
+  } else if (transactionFee > taxBands[3].range[0] && transactionFee < taxBands[3].range[1]) {
+    tax +=
+      calculateTax(taxBands[1].difference, taxBands[1].rate) +
+      calculateTax(taxBands[2].difference, taxBands[2].rate);
+
     taxableTransactionFee -= taxBands[1].difference + taxBands[2].difference;
-    tax += Math.floor(taxableTransactionFee * taxBands[3].rate);
+    tax += calculateTax(taxableTransactionFee, taxBands[3].rate);
 
     // band 4
   } else if (transactionFee > taxBands[4].range[0]) {
-    tax += Math.floor(
-      taxBands[1].difference * taxBands[1].rate +
-        taxBands[2].difference * taxBands[2].rate +
-        taxBands[3].difference * taxBands[3].rate
-    );
+    tax +=
+      calculateTax(taxBands[1].difference, taxBands[1].rate) +
+      calculateTax(taxBands[2].difference, taxBands[2].rate) +
+      calculateTax(taxBands[3].difference, taxBands[3].rate);
+
     taxableTransactionFee -=
       taxBands[1].difference + taxBands[2].difference + taxBands[3].difference;
 
-    tax += Math.floor(taxableTransactionFee * taxBands[4].rate);
+    tax += calculateTax(taxableTransactionFee, taxBands[4].rate);
   }
 
   return `£${tax.toFixed(2)}`;
